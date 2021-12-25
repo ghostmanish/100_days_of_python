9a01047e11f4254ece79a5399c12f6957766ee32
Manish Kumar Goswami <53654018+ghostmanish@users.noreply.github.com>
2021-12-25 12:49:48 +0530

    Day 1
    I Have created Band Name Generator using Python.

---
+++ Day_1_Band_Name_Generator.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 25 12:44:29 2021
+
+@author: Manish Kumar Goswami
+"""
+
+#Banner
+
+print("Welcome To the Band Name Generator")
+
+#Input of City Name and Pet Name
+
+city_name = input("What's name of the city you grew up in? \n")
+pet_name = input("What's your pet's name?\n")
+
+#OutPut
+
+print("Your band name could be "+ city_name + " " + pet_name)
